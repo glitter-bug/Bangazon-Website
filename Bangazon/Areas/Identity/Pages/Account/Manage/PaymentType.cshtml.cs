b808868b2c0c9c3b410e93fec9cb8cdcb71684b0
Deep-A_Patel <deep.ag123007@gmail.com>
2019-08-27 13:09:30 -0700

    still workinggggggg

--- Bangazon/Areas/Identity/Pages/Account/Manage/PaymentType.cshtml.cs
+++ Bangazon/Areas/Identity/Pages/Account/Manage/PaymentType.cshtml.cs
@@ -2,15 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bangazon.Data;
+using Bangazon.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bangazon.Areas.Identity.Pages.Account.Manage
 {
     public class PaymentTypeModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PaymentTypeModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
         }
+
+        // GET: PaymentTypes
+        
+
     }
 }
