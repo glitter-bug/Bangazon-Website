dadec9e5a3aabc70580060aba5be9a7ac63ecb08
godspeedless@gmail.com <godspeedless@gmail.com>
2019-08-27 12:23:41 -0700

    testing PR

--- Bangazon/Areas/Identity/Pages/Account/Manage/OrderHistory.cshtml.cs
+++ Bangazon/Areas/Identity/Pages/Account/Manage/OrderHistory.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bangazon.Data;
+using Bangazon.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +12,16 @@ namespace Bangazon.Areas.Identity.Pages.Account.Manage
 {
     public class OrderHistoryModel : PageModel
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public OrderHistoryModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
         public void OnGet()
         {
         }
