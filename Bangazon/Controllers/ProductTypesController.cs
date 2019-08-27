cb56fe04ffe693399bbd6f7332c3892a0811ea42
Deep-A_Patel <deep.ag123007@gmail.com>
2019-08-27 08:54:43 -0700

    productType detials now showing products baby

--- Bangazon/Controllers/ProductTypesController.cs
+++ Bangazon/Controllers/ProductTypesController.cs
@@ -38,6 +38,7 @@ namespace Bangazon.Controllers
             }
 
             var productType = await _context.ProductType
+                .Include(pt => pt.Products)
                 .FirstOrDefaultAsync(m => m.ProductTypeId == id);
             if (productType == null)
             {
