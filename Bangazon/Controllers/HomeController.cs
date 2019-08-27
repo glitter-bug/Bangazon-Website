6db3c079bea0ba6b8949fc40785c5d45c5b1a1fe
godspeedless@gmail.com <godspeedless@gmail.com>
2019-08-27 09:49:05 -0700

    new master

--- Bangazon/Controllers/HomeController.cs
+++ Bangazon/Controllers/HomeController.cs
@@ -5,13 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Bangazon.Models;
+using Bangazon.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bangazon.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products = await _context.Product
+                                        
+                                        .ToListAsync();
+                                         
             return View();
         }
 
